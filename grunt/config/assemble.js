bb08056202bd953298da41f3aec8beb4c72f7c3a
Dennis Haulund Nielsen <dhni@nodes.dk>
2016-01-28 10:56:26 +0100

    Relative assets for gh-pages branch

--- grunt/config/assemble.js
+++ grunt/config/assemble.js
@@ -16,7 +16,7 @@ module.exports = {
 	dist: {
 		options: {
 			flatten: false,
-			assets: '<%= paths.dist %>assets',
+			assets: '<%= paths.dist %>/assets',
 			data: ['<%= paths.doc %>data/*.json'],
 			helpers: ['<%= paths.doc %>helpers/*.js'],
 			partials: ['<%= paths.doc %>includes/**/*.{html,scss}'],
