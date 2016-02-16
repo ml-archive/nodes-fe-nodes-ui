a9749099890482be95c2d3898145f8e8f8b4f3f0
Dennis Haulund Nielsen <dhni@nodes.dk>
2016-02-16 11:03:30 +0100

    Updates

--- assets/js/nodes.js
+++ assets/js/nodes.js
@@ -631,6 +631,152 @@ function leftSidebarToggleClick() {
 	jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
 })(jQuery,'smartresize');
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+	"use strict";
+	/*
+	 * ...
+	 * Author: Dennis Haulund Nielsen <dhni@nodes.dk>
+	 */
+	var wysiwygWrapper = {
+		init: function( options, elem ) {
+			// Save the element reference, both as a jQuery
+			// reference and a normal reference
+			this.elem  = elem;
+			this.$elem = $(elem);
+
+			// Mix in the passed-in options with the default options
+			this.options = $.extend( {}, this.options, options );
+
+			this.wysiwygType = this.$elem.data('wysiwyg-type') || 'advanced';
+
+			// Mix in the data-options with the options
+			this.options = $.extend( {}, this.options, _evalDataOptions( this.$elem.data('options') ) );
+
+			console.log('init with ', this.wysiwygType, this.options[this.wysiwygType]);
+			// Initialize wysiwyg plugin
+			CKEDITOR.replace(this.$elem.attr('id'), this.options[this.wysiwygType]);
+
+			// return this so that we can chain and use the bridge with less code.
+			return this;
+
+		},
+		options: {
+			simple: {
+				format_tags: 'p;h1;h2;h3;h4;h5;h6',
+				'toolbar': [
+					{
+						name: 'document',
+						items: [ 'Source' ]
+					},
+					{
+						name: 'clipboard',
+						items: [ 'Undo', 'Redo' ]
+					},
+					{
+						name: 'tools',
+						items: [ 'Maximize', 'ShowBlocks' ]
+					},
+					{
+						name: 'clearformat',
+						items: [ 'RemoveFormat' ]
+					},
+					'/',
+					{
+						name: 'insert',
+						items: [ 'Table', 'HorizontalRule']
+					},
+					{
+						name: 'basicstyles',
+						items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ]
+					},
+
+					{
+						name: 'paragraph',
+						items: [ 'NumberedList', 'BulletedList', 'Blockquote' ]
+					},
+					{
+						name: 'links',
+						items: [ 'Link', 'Unlink', 'Anchor' ]
+					},
+					{
+						name: 'styles',
+						items: [ 'Format', 'FontSize' ]
+					},
+
+				]
+			},
+			advanced: {
+				format_tags: 'p;h1;h2;h3;h4;h5;h6;pre;address;div',
+				'toolbar': [
+					{
+						'name': 'clipboard',
+						'items': ['Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo']
+					}, {
+						'name': 'links',
+						'items': ['Link', 'Unlink']
+					}, {
+						'name': 'source',
+						'items': ['Source']
+					}, {
+						name: 'tools',
+						items: [ 'Maximize', 'ShowBlocks' ]
+					}, {
+						name: 'clearformat',
+						items: [ 'RemoveFormat' ]
+					}, '/', {
+						'name': 'basicstyles',
+						'groups': ['basicstyles'],
+						'items': ['Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript']
+					}, {
+						'name': 'paragraph',
+						'items': ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote']
+					}, {
+						'name': 'elements',
+						'items': ['Image', 'Table', 'HorizontalRule']
+					}, {
+						'name': 'styles',
+						'items': ['Styles', 'Format', 'Font', 'Fontsize', 'TextColor', 'BGColor']
+					}
+				]
+			}
+		}
+	};
+
+	/*
+	 Helper Methods
+	 */
+	function _evalDataOptions(options) {
+		options = eval('(' + options + ')');
+		return options;
+	}
+
+	// Object.create support test, and fallback for browsers without it
+	if ( typeof Object.create !== "function" ) {
+		Object.create = function (o) {
+			function F() {}
+			F.prototype = o;
+			return new F();
+		};
+	}
+
+	// Create a plugin based on a defined object
+	$.plugin = function( name, object ) {
+		$.fn[name] = function( options ) {
+			return this.each(function() {
+				if ( ! $.data( this, name ) ) {
+					$.data( this, name, Object.create(object).init(
+						options, this ) );
+				}
+			});
+		};
+	};
+
+	// Register the plugin
+	$.plugin('wysiwyg', wysiwygWrapper);
+
+})( jQuery, window, document );
 var Nodes = (function() {
 	return {
 		/**
@@ -947,8 +1093,16 @@ var Nodes = (function() {
 			});
 		},
 
+		/**
+		 * DEPRECATION WARNING:
+		 * This method will be deprecated in favor of the UMD modules introduced in 1.1+
+		 *
+		 * If you experience ANY issue instantiating through the new API feel free to go back to the old way.
+		 * Please report the bug though!
+		 */
 		wysiwyg: function(element) {
-			CKEDITOR.replace(element.attr('id'));
+			$(element).wysiwyg();
+			//CKEDITOR.replace(element.attr('id'));
 		},
 
 		// Set default configuration for all Chart.js charts
